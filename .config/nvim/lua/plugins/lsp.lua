df4d9a87de6bd6da9ac808fbe944c97fa608d202
Anton Rogov <anton@blackbits.pro>
2025-12-13 22:37:46 +0300

    rust-analyzer lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -16,7 +16,31 @@ return {
     enable_servers({
       eslint = {},
       jsonls = {},
-      rust_analyzer = {},
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              features = 'all',
+            },
+            checkOnSave = {
+              enable = true,
+            },
+            check = {
+              command = 'clippy',
+            },
+            imports = {
+              group = {
+                enable = false,
+              },
+            },
+            completion = {
+              postfix = {
+                enable = false,
+              },
+            },
+          },
+        },
+      },
       solargraph = {
         root_markers = { '.solargraph.yml' },
       },
